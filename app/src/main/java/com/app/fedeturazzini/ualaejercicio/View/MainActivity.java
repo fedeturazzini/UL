eb0aa924552c5bf30480fbb92baef6d1e77d2152
fedeturazzini <fede.turazzini@gmail.com>
2019-01-30 17:49:04 -0300

    Primer commit - Init repo

---
+++ app/src/main/java/com/app/fedeturazzini/ualaejercicio/View/MainActivity.java
@@ -0,0 +1,15 @@
+package com.app.fedeturazzini.ualaejercicio.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.fedeturazzini.ualaejercicio.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
